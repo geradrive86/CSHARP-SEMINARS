f338ca2ac3ce1c396ea0627a414d68740df48a9f
geradrive86 <geradrive86@gmail.com>
2024-10-03 01:57:59 +0300

    Added task02

--- Seminar1_DZ_1/Task_02/Program.cs
+++ Seminar1_DZ_1/Task_02/Program.cs
@@ -1,15 +1,31 @@
-﻿int min = Math.Min(Math.Min(num1, num2), num3);
-int max = Math.Max(Math.Max(num1, num2), num3);
+﻿// Задача №2
+// Напишите метод, который принимает на вход три числа и возвращает
+// среднее из этих чисел (то есть не самое большое и не самое маленькое).
 
-if (num1 != min && num1 != max)
+
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+
+if ((num1 >= num2 && num1 <= num3) || (num1 >= num3 && num1 <= num2))
 {
+    Console.WriteLine("Среднее число:" + num1);
     return num1;
 }
-else if (num2 != min && num2 != max)
+else if ((num2 >= num1 && num2 <= num3) || (num2 <= num1 && num2 >= num3))
 {
+    Console.WriteLine("Среднее число:" + num2);
     return num2;
 }
 else
-{
+{ 
+    Console.WriteLine("Среднее число:" + num3);
     return num3;
 }
