a7730dc27ed1a6e8b0344b6962cdcc441e9bb6a0
geradrive86 <geradrive86@gmail.com>
2024-10-03 00:43:24 +0300

    02

---
+++ Seminar1_DZ_1/Task_02/Program.cs
@@ -0,0 +1,15 @@
+﻿int min = Math.Min(Math.Min(num1, num2), num3);
+int max = Math.Max(Math.Max(num1, num2), num3);
+
+if (num1 != min && num1 != max)
+{
+    return num1;
+}
+else if (num2 != min && num2 != max)
+{
+    return num2;
+}
+else
+{
+    return num3;
+}
