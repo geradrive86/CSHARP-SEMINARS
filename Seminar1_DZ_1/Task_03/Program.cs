987141d050c9cbfa47aa3233143b84546d65efaf
geradrive86 <geradrive86@gmail.com>
2024-10-03 09:36:57 +0300

    Added Task_03

---
+++ Seminar1_DZ_1/Task_03/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №3
+// Напишите метод, который на вход принимает число и выводит, является ли
+// оно положительным (больше нуля), отрицательным (меньше нуля) или нулём.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 0)
+{
+Console.WriteLine("положительное");
+} 
+else if (num < 0)
+{
+Console.WriteLine("отрицательное");
+} 
+
+else
+{
+Console.WriteLine("ноль");
+}
