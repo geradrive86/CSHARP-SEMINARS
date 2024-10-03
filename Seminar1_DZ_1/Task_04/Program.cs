956bc1fae7e751f8390d10c359e3f9824b30f073
geradrive86 <geradrive86@gmail.com>
2024-10-03 22:43:25 +0300

    Added Task_04

---
+++ Seminar1_DZ_1/Task_04/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача №4 Напишите метод, который на вход принимает число (number), 
+// а на выходе выводит все нечетные числа от 1 до number (включительно), 
+// после каждого числа должен быть знак пробела.
+
+
+static void PrintOddNumbers(int number)
+{
+    for (int i = 1; i <= number; i += 2)
+    {
+        Console.Write(i + " ");
+    }
+}
+PrintOddNumbers(30);
