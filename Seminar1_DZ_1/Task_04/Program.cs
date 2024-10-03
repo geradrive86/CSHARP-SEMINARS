2a0f0c2c4416492d7e1896f2913e976f63ac23b3
geradrive86 <geradrive86@gmail.com>
2024-10-04 00:15:03 +0300

    Дополнение и исправление кода в задаче №4

--- Seminar1_DZ_1/Task_04/Program.cs
+++ Seminar1_DZ_1/Task_04/Program.cs
@@ -1,13 +1,19 @@
 ﻿// Задача №4 Напишите метод, который на вход принимает число (number), 
 // а на выходе выводит все нечетные числа от 1 до number (включительно), 
 // после каждого числа должен быть знак пробела.
-
-
+{
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    PrintOddNumbers(number);
+}
 static void PrintOddNumbers(int number)
 {
-    for (int i = 1; i <= number; i += 2)
+    for (int i = 1; i <= number; i++)
     {
-        Console.Write(i + " ");
+        if (i % 2 != 0) // Проверка на нечетное число
+        {
+            Console.Write(i + " "); // Выводим число и пробел
+        }
     }
+    Console.WriteLine(); // Переход на новую строку после вывода всех чисел
 }
-PrintOddNumbers(30);
