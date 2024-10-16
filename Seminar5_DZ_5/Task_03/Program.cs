d34524b4beaf67cb6f002cce161d4272cb639320
geradrive86 <geradrive86@gmail.com>
2024-10-16 17:41:44 +0300

    Added Seminar5_DZ_5

---
+++ Seminar5_DZ_5/Task_03/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задание 3: Транспонирование двумерного массива
+// Описание: Задайте двумерный массив целых чисел. Напишите программу, которая
+// транспонирует массив (меняет строки и столбцы местами).
+
+int[,] array = {
+            { 1, 2, 3 },
+            { 4, 5, 6 }
+        };
+int[,] transposed = new int[array.GetLength(1), array.GetLength(0)]; // Новый массив для транспонированных данных
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        transposed[j, i] = array[i, j]; // Перестановка строк и столбцов
+    }
+}
+PrintArray(transposed); // Выводим транспонированный массив
+static void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Seminar5_DZ_5/Task_04/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задание 4: Замена отрицательных элементов в двумерном массиве
+// Описание: Задайте двумерный массив. Замените все отрицательные числа на их
+// абсолютные значения
+
+int[,] matrix = {
+            {-1, 2, -3},
+            {4, -5, 6},
+            {-7, 8, -9}
+        };
+int rows = matrix.GetLength(0);
+int columns = matrix.GetLength(1);
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        if (matrix[i, j] < 0) // Если элемент отрицательный
+        {
+            matrix[i, j] = Math.Abs(matrix[i, j]); // Заменяем его абсолютным значением
+        }
+    }
+}
+Console.WriteLine("Матрица с абсолютными значениями отрицательных чисел:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
